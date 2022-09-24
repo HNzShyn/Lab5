5ae685932928655d5f5d08d2126db0636465fe62
HNzShyn <nvu1343@gmail.com>
2022-09-24 17:24:38 +0700

    ok

--- index.js
+++ index.js
@@ -13,13 +13,18 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
-    var name  = "Cloud Computing";
-    var code = 1644;
+    var name  = "Cloud Computing";//String
+    var code = 1644;//nt
+    //pass dữ liệu từ back-end sang front-end
+    //cú pháp: name (data) : value (data)
+    //gửi dữ liệu là value và nhận với tên là name
     res.render("index", {name: name, ma: code, ky: "Fall 2022"});
 })
 
 app.get("/mobile", (req, res) => {
-    res.render("mobile")
+    var list = ["Iphone", "SamSung", "Oppo", "Huawei"];
+
+    res.render("mobile", {mobile: list})
 })
 
 app.get("/laptop", (req, res) => {
